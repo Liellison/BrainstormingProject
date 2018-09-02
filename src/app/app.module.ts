3d42a893844edffa5c5eae0deee657cab7289f3c
Liellison Menezes <liellisonjoseph@gmail.com>
2018-09-02 00:24:17 -0300

    0.0.0.1-2: Inclusão de Rotas para navegação inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { LoginPageTenhoComponent } from './login-page-tenho/login-page-tenho.component';
+import { routes } from './app.router';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageTenhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
