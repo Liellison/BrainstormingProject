943052f360b8973be1092d029ae0089a507e960d
luccasmcarvalho <luccas.carvalho@gmail.com>
2018-09-23 09:56:55 -0300

    Adição do menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginPageTenhoComponent } from './login-page-tenho/login-page-tenho.component';
 import { SingupComponent } from './singup/singup.component';
 import { routes } from './app.router';
-<<<<<<< HEAD
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
-=======
-import { AutenticacaoService } from './login-page-tenho/autenticacao.service';
-import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { Bran1Component } from './tela-inicial/bran1/bran1.component';
-import { LoginErradoComponent } from './login-page-tenho/login-errado/login-errado.component';
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
->>>>>>> master
 
 
 @NgModule({
@@ -24,24 +14,13 @@ import { CabecalhoComponent } from './cabecalho/cabecalho.component';
     AppComponent,
     LoginPageTenhoComponent,
     SingupComponent,
-<<<<<<< HEAD
     MenuLateralComponent
-=======
-    TelaInicialComponent,
-    WelcomeComponent,
-    Bran1Component,
-    LoginErradoComponent,
-    CabecalhoComponent
->>>>>>> master
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    FormsModule
+    RouterModule.forRoot(routes)
   ],
-  providers: [
-    AutenticacaoService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
