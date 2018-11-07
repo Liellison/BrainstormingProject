b25264a2160f30cb849e149aa8d4cda63f5e91a5
luccasmcarvalho <39125512+luccasmcarvalho@users.noreply.github.com>
2018-11-07 14:01:58 -0300

    light-rating

--- src/app/pages/page-new-room/page-new-room.component.ts
+++ src/app/pages/page-new-room/page-new-room.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-page-new-room',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNewRoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+  SalasList: AngularFireList<any>;
+
+  formu = new FormGroup({
+    Descricao: new FormControl('', Validators.required),
+    Resumo: new FormControl('', Validators.required)
+  })
 
   ngOnInit() {
+  }
+  getSalas() {
+    this.SalasList = this.firebase.list('Salas');
+    return this.SalasList;
+  }
+
+  public salva(){
+    console.log(this.formu.value);
+    if(this.formu.valid){
+      this.createSalas(this.formu.valid);
+    }
+    alert("fuck");
+  }
+
+  public createSalas(sala){
+    if(!this.SalasList){
+      this.SalasList = this.getSalas();
+    }
+    
+    this.SalasList.push({
+      descricao: sala.Descricao,
+      resumo: sala.Resumo,
+      id: null,
+      lamps: null
+    });
   }
 
 }
