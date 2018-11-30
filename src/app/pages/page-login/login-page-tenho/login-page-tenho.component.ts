3fb3e471195c189e95e6a0d21b0fe31ddab4838b
luccasmcarvalho <luccas.carvalho@gmail.com>
2018-11-30 19:54:55 -0300

    login google button

--- src/app/pages/page-login/login-page-tenho/login-page-tenho.component.ts
+++ src/app/pages/page-login/login-page-tenho/login-page-tenho.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AutenticacaoService } from '../../../services/autenticacao.service';
 import { User } from '../../../services/User';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login-page-tenho',
   templateUrl: './login-page-tenho.component.html',
   styleUrls: ['./login-page-tenho.component.css']
 })
-export class LoginPageTenhoComponent implements OnInit{
-  LoginHas = 'Tenho cadastro'
+
+export class LoginPageTenhoComponent implements OnInit {
+  LoginHas = 'Tenho cadastro';
   public usuario: User = new User();
-  constructor(private autenticacao: AutenticacaoService){  }
+  constructor(private autenticacao: AutenticacaoService) {  }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
   }
 
-  doLogin(){
+  doLogin() {
     this.autenticacao.doLogin(this.usuario);
     console.log(this.usuario);
+
+    function onSignIn(googleUser) {
+      const profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
   }
+  // tslint:disable-next-line:prefer-const
+  // tslint:disable-next-line:member-ordering
+  provider = new firebase.auth.GoogleAuthProvider();
+
 }
